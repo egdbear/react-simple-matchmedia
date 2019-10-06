66f8204cbd4d880c3ef11a2effc03d640de47d4c
egdbear <egdfer@gmail.com>
2019-10-06 15:14:42 +0200

    Use hooks

--- index.js
+++ index.js
@@ -1,55 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 import mediaQueries from './mediaQueries';
 
-export default class MediaQuery extends React.Component {
-  mql = null;
+export default (query) => {
+  const queryToMatch = mediaQueries[query] || query;
+  const [matches, setMatches] = useState(window.matchMedia(queryToMatch).matches);
 
-  active = true;
+  useEffect(() => {
+    const media = window.matchMedia(queryToMatch);
+    if (media.matches !== matches) setMatches(media.matches);
+    const listener = () => setMatches(media.matches);
+    media.addListener(listener);
+    return () => media.removeListener(listener);
+  }, [matches, queryToMatch]);
 
-  state = {
-    matched: false,
-  }
-
-  componentDidMount() {
-    if (typeof window !== 'object') return;
-    if (!window.matchMedia) return;
-
-    const { media } = this.props;
-    const mediaToMatch = mediaQueries[media] || media;
-    this.mql = window.matchMedia(mediaToMatch);
-    this.mql.addListener(this.onMediaChange);
-
-    if (this.mql.matches) {
-      this.onMediaChange();
-    }
-  }
-
-  componentWillUnmount() {
-    this.active = false;
-    this.mql.removeListener(this.onMediaChange);
-    this.mql = null;
-  }
-
-  onMediaChange = () => {
-    if (!this.active) return;
-
-    this.setState({ matched: this.mql.matches });
-  }
-
-  render() {
-    const { children } = this.props;
-    const { matched } = this.state;
-
-    if (typeof children === 'function') {
-      return children(matched);
-    }
-
-    return matched ? children : null;
-  }
-}
-
-MediaQuery.propTypes = {
-  media: PropTypes.string.isRequired,
-  children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+  return matches;
 };
