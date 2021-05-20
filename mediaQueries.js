f4b81f22db444f3ecfacd9a0984ca47e646da396
Ana <64069789+nas9lug2z@users.noreply.github.com>
2021-05-20 20:46:17 +0200

    Improved media query resolutions and added p hone_landscape option

--- mediaQueries.js
+++ mediaQueries.js
@@ -1,5 +1,6 @@
 export default {
-  phone: '(min-width: 320px) and (max-width: 568px)',
-  tablet: '(min-width : 768px) and (max-width : 1024px)',
-  desktop: '(min-width : 1224px)',
+	phone_portrait: '(max-width: 567px)',
+	phone_landscape: '(min-width : 568px) and (max-width: 767px)',
+	tablet: '(min-width : 768px) and (max-width : 1024px)',
+	desktop: '(min-width : 1025px)',
 };
